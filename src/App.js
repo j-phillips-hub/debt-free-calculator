2509972c3a10357a0bd927b68eaeb2f43ef11102
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-11-26 15:57:35 -0600

    Add css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './styles/App.css';
 import Form from './components/Form';
 import PaymentHistory from './components/PaymentHistory';
 import ExtraPayments from './components/ExtraPayments';
+import './styles/App.css';
+import './styles/Responsive.css';
 
 function App() {
   return (
