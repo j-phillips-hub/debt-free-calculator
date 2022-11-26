4821f788e66942a326f7371507b9b8992687ac72
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-11-26 10:47:09 -0600

    Add styled to form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Form.css";
+import "../styles/Btn.css";
 
 class Form extends React.Component {
   constructor() {
@@ -10,31 +11,39 @@ class Form extends React.Component {
     return (
       <section className="FormSection">
         <form className="Form">
-          <div className="Input__Container">
+          <div className="Flex InputContainer">
             <label className="Form__Label" htmlFor="loan-amount">
               Loan amount
             </label>
-            <input className="" type="text" />
+            <input className="Form__Input" type="text" />
           </div>
 
-          <div className="Input__Container">
-            <label htmlFor="loan-term-years">Loan term in years</label>
-            <input type="text" />
+          <div className="Flex InputContainer">
+            <label className="Form__Label" htmlFor="loan-term-years">
+              Loan term in years
+            </label>
+            <input className="Form__Input" type="text" />
           </div>
 
-          <div>Or</div>
+          <div className="Seperator">Or</div>
 
-          <div className="Input__Container">
-            <label htmlFor="loan-term-months">Loan term in months</label>
-            <input type="text" />
+          <div className="Flex InputContainer">
+            <label className="Form__Label" htmlFor="loan-term-months">
+              Loan term in months
+            </label>
+            <input className="Form__Input" type="text" />
           </div>
 
-          <div className="Input__Container">
-            <label htmlFor="intrest-rate">Interest rate per year</label>
-            <input type="text" />
-          </div>
+          <div className="Flex IntrestRate">
+            <div className="Flex InputContainer">
+              <label className="Form__Label" htmlFor="intrest-rate">
+                Interest rate per year
+              </label>
+              <input className="Form__Input" type="text" />
+            </div>
 
-          <button>Calculate</button>
+            <button className="Btn Btn--Small Calculate">Calculate</button>
+          </div>
         </form>
       </section>
     );
