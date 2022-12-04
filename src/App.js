6cf3fc05fd6cad87a719b8dd12c6ffaf01f9d53a
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-12-04 14:04:19 -0600

    Remove component from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import MakeAPayment from './components/MakeAPayment';
-import PaymentInfo from './components/PaymentInfo';
 import './styles/App.css';
 import './styles/Responsive.css';
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <main id='app' className="app">
       <MakeAPayment />
-      {/* <PaymentInfo /> */}
     </main>
   );
 }
