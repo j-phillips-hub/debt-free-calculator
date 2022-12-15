e71bacc5eff0c8ce0aed1c8f893879b9ac069ecc
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-12-14 21:48:02 -0600

    Change CalculateBtn to arrow function

--- src/components/CalculateBtn.jsx
+++ src/components/CalculateBtn.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function CalculateBtn({ calculateValue }) {
-  return (
-    <button onClick={calculateValue} className="btn btn--small calculate">
-      Calculate
-    </button>
-  );
-}
+const CalculateBtn = ({ calculateValue }) => (
+  <button onClick={calculateValue} className="btn btn--small calculate">
+    Calculate
+  </button>
+);
 
 export default CalculateBtn;
